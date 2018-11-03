222291eab16f331719bf9d78940f0383d94a588c
Sklep <matusdubrava@gmail.com>
2018-11-03 21:13:40 +0100

    added pug templates

--- fundamentals/express-server/routes/shop.js
+++ fundamentals/express-server/routes/shop.js
@@ -2,9 +2,16 @@ const router = require('express').Router();
 const path = require('path');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-	res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+	const products = adminData.products;
+	res.render('shop', {
+		prods: products,
+		docTitle: 'Shop',
+		path: '/',
+		pageTitle: 'Shop'
+	});
 });
 
 module.exports = router;
