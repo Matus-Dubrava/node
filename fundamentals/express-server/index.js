669d392979871cd48ddd6e42ce4a4cd2bd84afd2
Sklep <matusdubrava@gmail.com>
2018-11-04 10:53:36 +0100

    added ejs templates

--- fundamentals/express-server/index.js
+++ fundamentals/express-server/index.js
@@ -4,19 +4,10 @@ const path = require('path');
 
 const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine(
-	'hbs',
-	expressHbs({
-		layoutsDir: 'views/layouts/',
-		defaultLayout: 'main',
-		extname: 'hbs'
-	})
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(express.static(path.join(__dirname, 'public')));
